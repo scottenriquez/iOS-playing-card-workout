43accb85f34f12874eae5c30356c367617682f0a
Scottie Enriquez <scottie@enriquez.xxx>
2020-07-07 11:56:00 -0500

    Adding basic layout

---
+++ PlayingCardWorkout/PlayingCardWorkout/CardSelectionViewController.swift
@@ -0,0 +1,17 @@
+//
+//  CardSelectionViewController.swift
+//  PlayingCardWorkout
+//
+//  Created by Scott Enriquez on 7/7/20.
+//  Copyright © 2020 Scott Enriquez. All rights reserved.
+//
+
+import UIKit
+
+class CardSelectionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
