c22fb9a4f2b4469cd19667778e0885f6416dcedf
Scottie Enriquez <scottie@enriquez.xxx>
2020-07-10 19:34:05 -0500

    Adding IBActions and IBOutlets for home screen; adding timer functionality

--- PlayingCardWorkout/PlayingCardWorkout/CardSelectionViewController.swift
+++ PlayingCardWorkout/PlayingCardWorkout/CardSelectionViewController.swift
@@ -9,9 +9,34 @@
 import UIKit
 
 class CardSelectionViewController: UIViewController {
-
+     
+    var timer: Timer!
+    var cards: [UIImageView] = []
+    @IBOutlet var cardImageView: UIImageView!
+    @IBOutlet var buttons: [UIButton]!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        for button in buttons {
+            button.layer.cornerRadius = 8
+        }
+        startTimer()
+    }
+    
+    func startTimer() {
+        timer = Timer.scheduledTimer(timeInterval: 0.5, target: self, selector: #selector(showRandomImage), userInfo: nil, repeats: true)
+    }
+    
+    @objc func showRandomImage() {
+        cardImageView = cards.randomElement()
     }
 
+    @IBAction func stopButtonPressed(_ sender: UIButton) {
+    }
+    
+    @IBAction func startButtonPressed(_ sender: UIButton) {
+    }
+
+    @IBAction func rulesButtonPressed(_ sender: UIButton) {
+    }
 }
